e898eec6fbfb101d5cf21541a3747915bee21ae1
Cristian Klein <cristian.klein@elastisys.com>
2025-04-28 22:07:21 +0200

    Fix TypeScript errors

--- src/MapView.tsx
+++ src/MapView.tsx
@@ -1,8 +1,14 @@
 import { MapContainer, TileLayer } from 'react-leaflet';
+import { LatLngExpression } from 'leaflet';
+
+const position: LatLngExpression = [55.60249267294951, 12.967599313254912];
 
 const MapView = () => {
   return (
-    <MapContainer center={[55.60249267294951, 12.967599313254912]} zoom={16} style={{ height: "100vh", width: "100%" }}>
+    <MapContainer
+      style={{ height: "100vh", width: "100%" }}
+      center={position}
+      zoom={16}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
