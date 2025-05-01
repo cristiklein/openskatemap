7a8983edc7fa34ea677b7a6a5a4f7a17aef9fa46
Cristian Klein <cristian.klein@elastisys.com>
2025-05-01 13:49:21 +0200

    Ensure to log DB connection failure

--- server/start.ts
+++ server/start.ts
@@ -1,7 +1,13 @@
 import { app, initDb } from './server';
+import logger from './logger';
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 initDb().then(() => {
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  app.listen(port, () => {
+    logger.info(`Server running on port ${port}`);
+  });
+}).catch((err: unknown) => {
+  logger.error({ err }, 'Failed to initialize the database');
+  process.exit(1);
 });
