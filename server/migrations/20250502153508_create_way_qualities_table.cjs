53a00228afda2dc717aff4cd59de63fe703b6ad4
Cristian Klein <cristian.klein@elastisys.com>
2025-05-02 17:48:47 +0200

    Make migration backwards-compatible

--- server/migrations/20250502153508_create_way_qualities_table.cjs
+++ server/migrations/20250502153508_create_way_qualities_table.cjs
@@ -3,18 +3,22 @@
  * @returns { Promise<void> }
  */
 exports.up = async function(knex) {
-  await knex.schema.createTable('way_qualities', (table) => {
-    table.increments('id').primary();
-    table.integer('way_id').notNullable();
-    table.integer('quality').notNullable();
-    table.timestamp('timestamp').notNullable();
-    table.string('ip');
-  });
+  await knex.schema.hasTable('way_qualities').then(async (exists) => {
+    if (!exists) {
+      await knex.schema.createTable('way_qualities', (table) => {
+        table.increments('id').primary();
+        table.integer('way_id').notNullable();
+        table.integer('quality').notNullable();
+        table.timestamp('timestamp').notNullable();
+        table.string('ip');
+      });
 
-  // Create index
-  await knex.schema.raw(`
-    CREATE INDEX IF NOT EXISTS idx_way_timestamp ON way_qualities (way_id, timestamp)
-  `);
+      // Create index
+      await knex.schema.raw(`
+        CREATE INDEX IF NOT EXISTS idx_way_timestamp ON way_qualities (way_id, timestamp)
+      `);
+    }
+  });
 };
 
 /**
