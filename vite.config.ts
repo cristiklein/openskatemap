982d1deb427bc4564df01336691f4ad1414c1fc5
Cristian Klein <cristian.klein@elastisys.com>
2025-05-02 22:49:32 +0200

    Fix base URL

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/openskatemap/',
   plugins: [
     react(),
     VitePWA({ registerType: 'autoUpdate' })
