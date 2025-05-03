abadd168aa3e874799c3fe67428c723830953934
Cristian Klein <cristian.klein@elastisys.com>
2025-05-03 17:14:34 +0200

    Add welcome screen as fallback

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,24 @@ import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 
+import { createHtmlPlugin } from 'vite-plugin-html';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+
+const welcomeHtml = readFileSync(resolve(__dirname, 'src/welcome.html'), 'utf-8');
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({ registerType: 'autoUpdate' })
+    VitePWA({ registerType: 'autoUpdate' }),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          welcomeContent: welcomeHtml,
+        },
+      },
+    }),
   ],
   define: {
     '__APP_VERSION__': JSON.stringify(process.env.npm_package_version),
