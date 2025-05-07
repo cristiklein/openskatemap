92d28804099b5c218458456488c0f5d049cf4cae
Cristian Klein <cristian.klein@elastisys.com>
2025-05-07 17:20:40 +0200

    Updates

--- assets/L.Control.Locate.esm-CVnutzL1.js
+++ assets/L.Control.Locate.esm-CVnutzL1.js
@@ -1,4 +1,4 @@
-import{l as e}from"./index-DLlbl992.js";/*!
+import{l as e}from"./index-BkNU-Qwb.js";/*!
 Copyright (c) 2016 Dominik Moritz
 
 This file is part of the leaflet locate control. It is licensed under the MIT license.
