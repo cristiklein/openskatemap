555536cdca7a33b06b3678167e493d1af883fe16
Cristian Klein <cristian.klein@elastisys.com>
2025-05-09 22:43:45 +0200

    Updates

--- assets/L.Control.Locate.esm-nFuip-eF.js
+++ assets/L.Control.Locate.esm-nFuip-eF.js
@@ -1,4 +1,4 @@
-import{l as e}from"./index-BfvrzyVM.js";/*!
+import{l as e}from"./index-md9HZFVw.js";/*!
 Copyright (c) 2016 Dominik Moritz
 
 This file is part of the leaflet locate control. It is licensed under the MIT license.
