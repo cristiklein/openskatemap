0a39dc13f45896829b287aa73ea8c187cfe8dd90
Cristian Klein <cristian.klein@elastisys.com>
2025-05-09 22:01:14 +0200

    test(migrations): Make TypeScript happy

--- server/migrations.test.ts
+++ server/migrations.test.ts
@@ -1,12 +1,12 @@
-import knex from 'knex';
+import knex, { Knex } from 'knex';
 import knexConfig from './knexfile';
 import { beforeAll, afterAll, describe, it, expect } from 'vitest';
 
-let db;
+let db : Knex;
 
 beforeAll(async () => {
   db = knex(knexConfig); // use a test config, e.g., SQLite memory
-  await db.migrate.rollback(null, true); // reset all
+  await db.migrate.rollback(undefined, true); // reset all
   await db.migrate.latest(); // apply migrations
 });
 
@@ -34,8 +34,8 @@ describe('Migrations', () => {
   });
 
   it('preserves data during migration', async () => {
-    await db.migrate.rollback(null, true);
-    await db.migrate.up('20250502153508_create_way_qualities_table');
+    await db.migrate.rollback(undefined, true);
+    await db.migrate.up({ name: '20250502153508_create_way_qualities_table.cjs' });
     await db('way_qualities').insert({ way_id: 111, quality: 0, timestamp: Date.now()});
     await db.migrate.latest();
 
@@ -43,7 +43,7 @@ describe('Migrations', () => {
     expect(wq.way_id).toBe(111);
     expect(wq.quality).toBe(0);
 
-    await db.migrate.down('20250502153508_create_way_qualities_table');
+    await db.migrate.down();
 
     const wq2 = await db('way_qualities').first();
     expect(wq2.way_id).toBe(111);
